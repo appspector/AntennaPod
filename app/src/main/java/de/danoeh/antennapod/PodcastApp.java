6eaa7861caaff83f34ceb2a553caeda0ebc5543f
PavelBjorn <PavelBjorn@gmail.com>
2018-05-17 14:53:56 +0300

    Revert "AP-1244 Integrate Android SDK into AntennaPod (#1)"
    This reverts commit f8f8c7a34e72abe8a03f114a2ee7760cb563d70c.

--- app/src/main/java/de/danoeh/antennapod/PodcastApp.java
+++ app/src/main/java/de/danoeh/antennapod/PodcastApp.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.os.Build;
 import android.os.StrictMode;
 
-import com.appspector.sdk.AppSpector;
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
 import com.joanzapata.iconify.fonts.MaterialModule;
@@ -13,9 +12,7 @@ import de.danoeh.antennapod.core.ClientConfig;
 import de.danoeh.antennapod.core.feed.EventDistributor;
 import de.danoeh.antennapod.spa.SPAUtil;
 
-/**
- * Main application class.
- */
+/** Main application class. */
 public class PodcastApp extends Application {
 
     // make sure that ClientConfigurator executes its static code
@@ -27,48 +24,40 @@ public class PodcastApp extends Application {
         }
     }
 
-    private static PodcastApp singleton;
+	private static PodcastApp singleton;
 
-    public static PodcastApp getInstance() {
-        return singleton;
-    }
+	public static PodcastApp getInstance() {
+		return singleton;
+	}
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
+	@Override
+	public void onCreate() {
+		super.onCreate();
 
-        Thread.setDefaultUncaughtExceptionHandler(new CrashReportWriter());
+		Thread.setDefaultUncaughtExceptionHandler(new CrashReportWriter());
 
-        if (BuildConfig.DEBUG) {
-            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder()
-                    .detectLeakedSqlLiteObjects()
-                    .penaltyLog()
-                    .penaltyDropBox();
-            builder.detectActivityLeaks();
-            builder.detectLeakedClosableObjects();
-            if (Build.VERSION.SDK_INT >= 16) {
-                builder.detectLeakedRegistrationObjects();
-            }
-            StrictMode.setVmPolicy(builder.build());
+		if(BuildConfig.DEBUG) {
+			StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder()
+				.detectLeakedSqlLiteObjects()
+				.penaltyLog()
+				.penaltyDropBox();
+			builder.detectActivityLeaks();
+			builder.detectLeakedClosableObjects();
+			if(Build.VERSION.SDK_INT >= 16) {
+				builder.detectLeakedRegistrationObjects();
+			}
+			StrictMode.setVmPolicy(builder.build());
+		}
 
-            initAppSpector();
-        }
+		singleton = this;
 
-        singleton = this;
+		ClientConfig.initialize(this);
 
-        ClientConfig.initialize(this);
-
-        EventDistributor.getInstance();
-        Iconify.with(new FontAwesomeModule());
-        Iconify.with(new MaterialModule());
+		EventDistributor.getInstance();
+		Iconify.with(new FontAwesomeModule());
+		Iconify.with(new MaterialModule());
 
         SPAUtil.sendSPAppsQueryFeedsIntent(this);
-    }
-
-    public void initAppSpector() {
-        AppSpector.build(this)
-                .withDefaultMonitors()
-                .run(BuildConfig.APPSPECTOR_API_KEY);
     }
 
 }
--- app/src/main/java/de/danoeh/antennapod/activity/ImportExportActivity.java
+++ app/src/main/java/de/danoeh/antennapod/activity/ImportExportActivity.java
@@ -13,11 +13,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
-
 import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.core.preferences.UserPreferences;
 import de.danoeh.antennapod.core.storage.PodDBAdapter;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
@@ -126,8 +124,9 @@ public class ImportExportActivity extends AppCompatActivity {
         d.setCancelable(false);
         d.setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {
             Intent intent = new Intent(getApplicationContext(), SplashActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+            ComponentName cn = intent.getComponent();
+            Intent mainIntent = IntentCompat.makeRestartActivityTask(cn);
+            startActivity(mainIntent);
         });
         d.show();
     }
